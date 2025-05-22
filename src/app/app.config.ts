03d819e8647f0c4cdd1c32fd348d27c915cf0d83
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-22 11:09:42 +0500

    refactor: removed some imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,10 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  importProvidersFrom,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { routes } from './app.routes';
 
 registerLocaleData(en);
 
@@ -20,8 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNzI18n(en_US),
-    importProvidersFrom(FormsModule),
-    provideAnimationsAsync(),
     provideHttpClient(),
   ],
 };
