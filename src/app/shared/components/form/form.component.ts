c8fd4267fd5df1e011a0c2510f67ae078507453a
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-22 10:05:29 +0500

    feat: form component added

---
+++ src/app/shared/components/form/form.component.ts
@@ -0,0 +1,40 @@
+import type { FormGroup } from '@angular/forms';
+
+import { Component, input } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+
+@Component({
+  selector: 'app-form',
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NzFormModule,
+    NzButtonModule,
+    NzTypographyModule,
+  ],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+export class FormComponent {
+  formGroup = input.required<FormGroup>();
+  submitFn = input.required<() => void>();
+  headline = input<string>();
+  btnName = input<string>('Submit');
+
+  submitForm() {
+    if (this.formGroup().valid) {
+      this.submitFn()();
+    } else {
+      Object.values(this.formGroup().controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+}
