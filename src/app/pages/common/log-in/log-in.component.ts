1d61cdefecc164c5a1936532accde286016a8b35
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-22 18:48:47 +0500

    feat: log in page updated

--- src/app/pages/common/log-in/log-in.component.ts
+++ src/app/pages/common/log-in/log-in.component.ts
@@ -1,18 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import {
-  NonNullableFormBuilder,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '@core/services/auth.service';
 
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { FormComponent } from '@component/form/form.component';
-import { InputPasswordComponent } from '@component/input-password/input-password.component';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { FormComponent } from '../../../shared/components/form/form.component';
+import { InputPasswordComponent } from '../../../shared/components/input-password/input-password.component';
 
 import { Page } from '@constant/page.constant';
+import { QueryUrl } from '@constant/query-url.constant';
 
 @Component({
   selector: 'log-in-page',
@@ -25,19 +24,35 @@ import { Page } from '@constant/page.constant';
     FormComponent,
     InputPasswordComponent,
   ],
+  providers: [AuthService],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css',
 })
 export class LogInComponent {
-  private readonly fb = inject(NonNullableFormBuilder);
+  constructor(
+    private readonly authService: AuthService,
+    private readonly notification: NzNotificationService,
+  ) {}
+
+  private readonly formBuilder = inject(NonNullableFormBuilder);
+  private readonly route = inject(ActivatedRoute);
+  private readonly fromPage: string = this.route.snapshot.queryParams[QueryUrl.FromPage] ?? '';
   readonly signUpHref = Page.SignUp;
 
-  validateForm = this.fb.group({
-    username: this.fb.control('', [Validators.required]),
-    password: this.fb.control('', [Validators.required]),
+  validateForm = this.formBuilder.group({
+    username: this.formBuilder.control('', [Validators.required]),
+    password: this.formBuilder.control('', [Validators.required]),
   });
 
   submitForm() {
-    console.log(this.validateForm.value);
+    if (this.validateForm.valid) {
+      const { username, password } = this.validateForm.value;
+      if (!username || !password) return;
+
+      this.authService.logIn(
+        { username, password },
+        { navigateTo: this.fromPage, notification: this.notification },
+      );
+    }
   }
 }
