49ff131db94373ccffc17588c6fcca5c3270c21c
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-22 17:10:42 +0500

    feat: app config updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import en from '@angular/common/locales/en';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { routes } from './app.routes';
@@ -13,6 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNzI18n(en_US),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
   ],
 };
