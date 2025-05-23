65cce4287390dfb0c3c7812ac7069b632aa60a96
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-23 09:37:23 +0500

    refactor: content component refactored

--- src/app/shared/components/content/content.component.ts
+++ src/app/shared/components/content/content.component.ts
@@ -5,6 +5,9 @@ import { Component, input } from '@angular/core';
   imports: [],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css',
+  host: {
+    '[class.full_height]': 'fullHeight()',
+  },
 })
 export class ContentComponent {
   readonly fullHeight = input(false);
