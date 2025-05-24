5a74a492d2586bd8eaf5a45ac9053234ea62673c
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-24 17:33:05 +0500

    feat: user service added

---
+++ src/app/core/services/user/user.service.ts
@@ -0,0 +1,31 @@
+import type { UserModel } from '@core/models/user.model';
+
+import { tap } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Endpoint } from '@constant/endpoint.constant';
+import { showHttpErrorMessage } from '@helper/http-err-message.helper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private readonly http: HttpClient) {}
+
+  getMyInfo(notification?: NzNotificationService) {
+    return this.http.get<UserModel>(Endpoint.UserMe).pipe(
+      tap({
+        error: err => showHttpErrorMessage(err, notification),
+      }),
+    );
+  }
+
+  get(id: number) {}
+
+  getAll() {}
+
+  update() {}
+
+  delete() {}
+}
