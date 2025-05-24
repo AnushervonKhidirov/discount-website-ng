d3414f9e6f457f8571f986ebc06f311fc6da46b5
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-24 17:47:47 +0500

    feat: header added

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,41 @@
+import type { NavigationModel } from '@core/models/navigation.model';
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainComponent } from '@layout/main/main.component';
+import { ContentComponent } from '@component/content/content.component';
+import { LogoComponent } from '@component/logo/logo.component';
+import { NavigationComponent } from '@component/navigation/navigation.component';
+import { AuthButtonsComponent } from '@component/auth-buttons/auth-buttons.component';
+
+import { Page } from '@constant/page.constant';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterOutlet,
+    MainComponent,
+    ContentComponent,
+    LogoComponent,
+    NavigationComponent,
+    AuthButtonsComponent,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  readonly menuItems: NavigationModel[] = [
+    {
+      title: 'Promotions',
+      href: Page.Promotion,
+    },
+    {
+      title: 'Stores',
+      href: Page.Store,
+    },
+    {
+      title: 'Companies',
+      href: Page.Company,
+    },
+  ];
+}
