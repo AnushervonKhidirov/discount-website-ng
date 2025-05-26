4c48a190adb5d58d3a545549377b0866baf35546
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-26 16:12:53 +0500

    feat: get user info on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Store } from '@ngrx/store';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { UserService } from '@core/services/user/user.service';
+import { setUserInfo } from '@core/store/user/user.actions';
+import { selectUser } from '@core/store/user/user.selectors';
+import { CookieKey } from '@constant/cookie.constant';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [UserService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    private readonly cookieService: CookieService,
+    private readonly userService: UserService,
+    private readonly notification: NzNotificationService,
+    private readonly store: Store,
+  ) {}
+
+  isFetched = signal(false);
+
+  ngOnInit() {
+    this.getUserInfo();
+  }
+
+  getUserInfo() {
+    const isTokenExists = this.cookieService.check(CookieKey.AccessToken);
+    if (!isTokenExists) return this.isFetched.set(true);
+
+    this.store.select(selectUser).subscribe(({ user }) => {
+      if (user) return this.isFetched.set(true);
+
+      this.userService.getMyInfo(this.notification).subscribe(user => {
+        this.store.dispatch(setUserInfo(user));
+        this.isFetched.set(true);
+      });
+    });
+  }
+}
