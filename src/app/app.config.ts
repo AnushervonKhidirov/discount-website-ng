fb79e01e27a5c9b3a915505e9880b5cfdcceecde
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-26 16:12:19 +0500

    feat: store added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import en from '@angular/common/locales/en';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { routes } from './app.routes';
 
 import { AuthInterceptor } from '@core/interceptors/auth/auth.interceptor';
+import { userReducer } from '@core/store/user/user.reducer';
 
 registerLocaleData(en);
 
@@ -18,5 +21,7 @@ export const appConfig: ApplicationConfig = {
     provideNzI18n(en_US),
     provideHttpClient(withFetch(), withInterceptors([AuthInterceptor])),
     provideAnimationsAsync(),
+    provideStore({ user: userReducer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
