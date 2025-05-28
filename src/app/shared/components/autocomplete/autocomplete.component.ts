ce4b5626020ba6f014dedd059d307fa2aa0a9733
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-28 21:33:23 +0500

    feat: autocomplete updated

--- src/app/shared/components/autocomplete/autocomplete.component.ts
+++ src/app/shared/components/autocomplete/autocomplete.component.ts
@@ -1,10 +1,14 @@
-import { Component, input } from '@angular/core';
-
+import { Component, input, AfterContentChecked } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+
+export type Options = {
+  id: number;
+  value: string | number;
+};
 
 @Component({
   selector: 'autocomplete',
@@ -12,13 +16,33 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
   templateUrl: './autocomplete.component.html',
   styleUrl: './autocomplete.component.css',
 })
-export class AutocompleteComponent {
+export class AutocompleteComponent implements AfterContentChecked {
   formGroup = input.required<FormGroup>();
   name = input.required<string>();
-  options = input.required<{ id: number; value: string | number }[]>();
-  onChange = input.required<(value: string | number) => void>();
-
+  options = input.required<Options[]>();
   label = input<string>();
-  clear = input<() => void>();
-  value = null;
+  placeholder = input<string>('');
+
+  value = '';
+  isLoaded = false;
+  filteredOptions: Options[] = [];
+
+  ngAfterContentChecked(): void {
+    if (this.options().length > 0 && !this.isLoaded) {
+      this.isLoaded = true;
+      this.filteredOptions = this.options();
+    }
+  }
+
+  onChangeHandler(value: string | number) {
+    const valueSting = value.toString();
+
+    this.filteredOptions = this.options().filter(option => {
+      return option.value.toString().toLowerCase().includes(valueSting.toLowerCase());
+    });
+  }
+
+  clearHandler() {
+    this.value = '';
+  }
 }
