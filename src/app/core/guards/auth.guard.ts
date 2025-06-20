371b3f9d9b06a6019741fc85cb7398939e9be8b0
Anushervon Khidirov <ankhid98@gmail.com>
2025-06-20 15:41:21 +0500

    fix: auth guard fixed

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,19 +1,16 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { selectUser } from '@core/store/user/user.selectors';
+import { CookieService } from 'ngx-cookie-service';
 
 import { Page } from '@constant/page.constant';
+import { CookieKey } from '@constant/cookie.constant';
 import { QueryUrl } from '@constant/query-url.constant';
 
 export const AuthGuard: CanActivateFn = (route, state) => {
+  const cookieService = inject(CookieService);
   const router = inject(Router);
-  const store = inject(Store);
-  let canActivate = false;
-
-  store.select(selectUser).subscribe(userStore => {
-    if (userStore.user) canActivate = true;
-  });
+  const accessToken = cookieService.get(CookieKey.AccessToken);
+  let canActivate = !!accessToken;
 
   if (!canActivate) {
     const loginPath = router.parseUrl(`${Page.LogIn}?${QueryUrl.FromPage}=${state.url}`);
