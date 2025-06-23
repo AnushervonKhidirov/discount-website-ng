778299134fb277552985095cf98c583d86b42931
Anushervon Khidirov <ankhid98@gmail.com>
2025-06-23 14:22:33 +0500

    feat: profile routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
-
 import { AuthGuard } from '@core/guards/auth.guard';
-
 import { HeaderComponent } from '@layout/header/header.component';
 
+// public pages
 import { LogInComponent } from '@page/public/log-in/log-in.component';
 import { SignUpComponent } from '@page/public/sign-up/sign-up.component';
 import { PromotionComponent } from '@page/public/promotion/promotion.component';
 import { CompanyComponent } from '@page/public/company/company.component';
-import { CreateCompanyComponent } from '@page/public/create-company/create-company.component';
 import { StoreComponent } from '@page/public/store/store.component';
+
+// private pages
+import { CreateCompanyComponent } from '@page/private/create-company/create-company.component';
+import { ProfileComponent } from '@page/private/profile/profile.component';
 
 export const routes: Routes = [
   {
@@ -27,7 +29,12 @@ export const routes: Routes = [
       },
       {
         path: 'stores',
-        component: StoreComponent,
+        children: [
+          {
+            path: '',
+            component: StoreComponent,
+          },
+        ],
       },
       {
         path: 'companies',
@@ -36,19 +43,29 @@ export const routes: Routes = [
             path: '',
             component: CompanyComponent,
           },
+        ],
+      },
+      {
+        path: 'profile',
+        canActivate: [AuthGuard],
+        children: [
           {
-            path: 'create',
-            component: CreateCompanyComponent,
-            canActivate: [AuthGuard],
+            path: '',
+            component: ProfileComponent,
           },
           {
-            path: 'my',
-            component: CompanyComponent,
-            canActivate: [AuthGuard],
-          },
-          {
-            path: ':id',
-            component: CompanyComponent, // NOTE: user specific component page
+            path: 'companies',
+            children: [
+              {
+                path: '',
+                component: CompanyComponent,
+                data: { profile: true },
+              },
+              {
+                path: 'create',
+                component: CreateCompanyComponent,
+              },
+            ],
           },
         ],
       },
