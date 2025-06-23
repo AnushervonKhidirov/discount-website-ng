9732cf79f3f306f10d50e75c8899fc1099a385e7
Anushervon Khidirov <ankhid98@gmail.com>
2025-06-23 16:27:47 +0500

    feat: card handler function added

--- src/app/shared/components/company-card/company-card.component.ts
+++ src/app/shared/components/company-card/company-card.component.ts
@@ -2,7 +2,7 @@ import type { CompanyModel } from '@core/models/company.model';
 
 import { Component, input } from '@angular/core';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { NzCardComponent } from 'ng-zorro-antd/card';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { RatingComponent } from '@component/rating/rating.component';
 
@@ -10,16 +10,27 @@ import { UploadUrlPipe } from '@core/pipes/upload-url.pipe';
 
 @Component({
   selector: 'company-card',
-  imports: [NzTypographyModule, NzCardComponent, NzDividerModule, RatingComponent, UploadUrlPipe],
+  imports: [NzTypographyModule, NzCardModule, NzDividerModule, RatingComponent, UploadUrlPipe],
   templateUrl: './company-card.component.html',
   styleUrl: './company-card.component.css',
 })
 export class CompanyCardComponent {
   readonly company = input.required<CompanyModel>();
   readonly showRating = input(false);
+  readonly cardHandler = input<(company: CompanyModel) => void>();
+
+  isHoverable = false;
   companyData: CompanyModel | null = null;
 
   ngOnInit() {
     this.companyData = this.company();
+    this.isHoverable = !!this.cardHandler();
+  }
+
+  handler() {
+    if (this.companyData) {
+      const fn = this.cardHandler();
+      if (fn) fn(this.companyData);
+    }
   }
 }
